16ce7d655692eed90f06071bdf6698ea85a775ee
Andrew Goncharov <71984230+Gazuz33@users.noreply.github.com>
2021-03-14 18:48:09 +0300

    fix: fix mistake (ver 2)

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 
 module.exports = function towelSort (matrix) {
-    if ( matrix !=0 && matrix.length != 0) {
+    if ( matrix ! = 0 && matrix.length ! = 0) {
         let Arr = [];
       for (var i = 0; i < matrix.length; i++) {
           if ( i % 2 == 1) {
-              Arr = Arr.concat(matrix[i]).reverse());
+              Arr = Arr.concat(matrix[i].reverse());
           }
            else Arr = Arr.concat(matrix[i]);
           }
